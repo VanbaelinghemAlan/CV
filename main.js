de316a0ebf6468f770dd90847b84b5da5f766e03
Vanbaelinghem Alan <Vanbaelinghem.Alan@gmail.com>
2022-06-22 17:00:24 +0200

    Info

---
+++ main.js
@@ -0,0 +1,7 @@
+const mainButton = document.getElementById ("btn-list")
+
+mainButton.addEventListener("click, toggleText")
+
+function toggleText(event){
+    this.classList.toggle("display")
+} //tentative de début de Javascript, mais pas le temps !//
